b12bda29c349aaa2654b78717ff271a441011f53
Youyi Ruan <youyir@nju.edu.cn>
2018-04-30 15:44:50 +0800

    clean up code for modernity SurfaceProgs/phaseVelInversion/rdsetupsimul.1.JdF.f

--- SurfaceProgs/phaseVelInversion/rdsetupsimul.1.JdF.f
+++ SurfaceProgs/phaseVelInversion/rdsetupsimul.1.JdF.f
@@ -24,29 +24,34 @@ c  Current version does not make station corrections.
       integer*4 nfreq,nstapts(maxnsta),idnum(maxnsta)
       integer*4 istanum(maxnsta),istacor(maxnsta), nevents
       character*70 foutput, fsummary, fstalist
+
 c   modify here       
       character*125 fn(maxevnts,maxnsta)
       character*70 finvrsnodes, fftoutput, fvariance,fmaxavamp
       character*70 ftemp,fvelarea
       character*16 staampcor
-      character*60 dummy1
+c      character*69 dummy1   ! cutting length depends on the path
       character*2 nettemp
-      character*4 statemp,staname(maxnsta)
+      character*4 statemp, staname(maxnsta)
       character*8 statemp2
+
+      logical debug
+      debug = .true.
+
       pi = 3.1415928
       convdeg = 3.1415928/180.
       circ = 6371.*3.1415928/180.
       twopi = 3.1415928*2.
+
 c  **************************************************
 c  WARNING:  The following two statements need to be switched depending on which
 c  compiler is used
 c  ************************************************
 
-200         format(a75, i2)
-201         format(a70, a2)
-202         format(a60, a4)
-c210	    format(a49, i2)
-
+c200         format(a75, i2)
+c201         format(a70, a2)
+c202         format(a69, a4)
+c210	     format(a49, i2)
 c200         format(1H ,a44, i2)
 c200         format(a44, i2)
 
@@ -56,19 +61,20 @@ c      data(fattnfac(i,1), fattnfac(i,2), i=1,6)/0.05, 0.15e-3, 0.025,
 c     * 0.15e-3, 0.01667, 0.15e-3, 0.01429, 0.1e-3, 0.0125, 0.05e-3, 
 c     * 0.0111, 0.05e-3/  
 
-	cattnfac=0.25e-3
+      cattnfac=0.25e-3
 
 c  read list of files and frequencies to be analyzed and file to output results
 c  Usually will pipe in data from some file like fasearrayinp
       read(*,*) nevents
-      nobs = 0
+c      nobs = 0
+
       do iev = 1, nevents
-        read(*,*) nsta(iev),idnum(iev)
-        nobs = nobs+ 2*nsta(iev)
+        read(*,*) nsta(iev), idnum(iev)
+c        nobs = nobs + 2*nsta(iev)
         read(*,'(a)') (fn(iev,i), i=1,nsta(iev))
-c	write(14,'(a)') (fn(iev,i), i=1,nsta(iev))
-c        write(*,*) iev, nsta(iev),idnum(iev)
+c       if(debug) write(*,*) iev, nsta(iev), idnum(iev)
       enddo
+
 c  a lot of the following input is unneeded, just here to parallel simannerr#
 c  input, so can use same fasearrayinp# or eqlistper# file for both
       read(*,*) nfreq
@@ -83,7 +89,7 @@ c  input, so can use same fasearrayinp# or eqlistper# file for both
       read(*,*) fvelarea
       read(*,*) fstalist
       read(*,*) unifvel
-      read(*,*) iterlimit, dampvel,dampaniso
+      read(*,*) iterlimit, dampvel, dampaniso
 
 c      idot=dot(foutput)
 c      write(*,*) idot
@@ -95,6 +101,7 @@ c      open(12, file = staampcor, status='old')
       open(13, file = ftemp)
       open(14, file = 'followit13')
       open(18, file = fstalist)
+
 c  fetch station corrections for amplitudes and assign to correction array
 c      read(12,*) nstacor
 c      do i = 1,nstacor
@@ -119,12 +126,15 @@ c     1   (fattnfac(j,2)-fattnfac(j+1,2))/(fattnfac(j,1)-
 c     1   fattnfac(j+1,1))
 c	end if
 c       enddo		!j
+
 c       if (freq(i).gt.fattnfac(1,1)) then
-c	attnfac(i) = fattnfac(1,2)
-c       end if 	
-c       if (freq(i).lt.fattnfac(6,1)) then
-c	attnfac(i) = fattnfac(6,2)
+c	  attnfac(i) = fattnfac(1,2)
 c       end if
+
+c       if (freq(i).lt.fattnfac(6,1)) then
+c	  attnfac(i) = fattnfac(6,2)
+c       end if
+
 c	write(*,*) "attnfac:  ", attnfac(i)
 c      enddo		!i 	
 
@@ -132,71 +142,75 @@ c  generate attenuation factor assuming Q = 100 and vel = 3.80km/s
 c       do i = 1, nfreq
 c         attnfac(i) = pi*freq(i)/380.
 c       enddo
+
 c  new attenuation factor based on 25 s inversion with multipathing
 c  taken into account is much smaller with Q about 385.  Average 
 c  velocity more like 3.80
+
 c	do i = 1, nfreq
 c           attnfac(i) = pi*freq(i)/(3.80*385.)
 c       enddo
 
 c  first, read in master list of stations    
       do ista2 = 1, maxnsta
-	read(18,*) staname(ista2)
-	if (staname(ista2).eq.'nope') then
-	  mxnsta = maxnsta -1
-	  go to 1111
-	endif
+        read(18,*) staname(ista2)
+        if(debug) write(*,*) ista2, staname(ista2)
+        if (staname(ista2).eq.'nope') then
+          mxnsta = maxnsta - 1
+          continue 
+          go to 1111
+        endif
       enddo
 1111  continue
 
 c  start input loop over events
       do iev = 1, nevents
         write(14,*) idnum(iev), nsta(iev)
+
         do ista = 1, nsta(iev)
+
           call rsac1(fn(iev,ista),tdata,nstapts(ista),beg(iev,ista),
      1       delt(ista),maxpts,nerr)
+
           call getfhv('DIST',stadist(iev,ista),nerr)
           call getfhv('AZ', staazi(iev,ista),nerr)
           call getfhv('BAZ', bazi(iev,ista),nerr)
           call getfhv('GCARC', stadelt(iev,ista), nerr)
           call getfhv('STLA', stalat(iev,ista),nerr)
           call getfhv('STLO', stalon(iev,ista),nerr)
-	  call getkhv('KSTNM', statemp2,nerr)
-	  rembazi = bazi(iev,1)
-	  remdist = stadist(iev,1)
-	  if ((abs(rembazi- bazi(iev,ista)).gt.4.0).or.
-     1       (abs(remdist-stadist(iev,ista)).gt.600.)) then    
-c	  write(14,*) bazi(iev,ista),stadist(iev,ista),rembazi,remdist
-c	  write(14,*) fn(iev,ista)
-	  endif
-	  
+          call getkhv('KSTNM', statemp2,nerr)
+
+          rembazi = bazi(iev,1)
+          remdist = stadist(iev,1)
+
+          if ((abs(rembazi-   bazi(iev,ista)).gt.4.0).or.
+     1        (abs(remdist-stadist(iev,ista)).gt.600.)) then    
+            write(14,*) bazi(iev,ista),stadist(iev,ista),rembazi,remdist
+            write(14,*) fn(iev,ista)
+          endif
+          
           geomsprd(ista) = sqrt(sin(stadelt(iev,ista)*convdeg))
-c  do following silly steps to extract station number from filename
-          write(13,'(a)') fn(iev,ista)
-c	  write(*,*) fn(iev,ista)
-          rewind (13)
-      
+
+          statemp = trim(statemp2)
+          write(14,*) statemp2, statemp
+
+          istanum(ista) = 0
+          
+          do ista2 = 1, mxnsta
+             if (statemp.eq.staname(ista2)) then
+               istanum(ista) = ista2
+               write(*,2070)stalat(iev,ista),stalon(iev,ista),statemp,ista2
+             endif
+          enddo
+2070   format(f10.4, f10.4,2x, a4, 2x, i3)
+
+          if (istanum(ista).eq.0) then
+            write(*,*) statemp, ista
+            write(*,*) 'WARNING ', statemp,' not in station list'
+          endif
+  
 c ********************************modify here***********************************
-	  read (13,202) dummy1, statemp
-	  rewind (13)
-	  write(14,*) statemp,statemp2
-	  istanum(ista) = 0
-	  do ista2 = 1, mxnsta
-	     if (statemp.eq.staname(ista2)) then
-	       istanum(ista) = ista2
-c	       write(*,2070)stalat(iev,ista), stalon(iev,ista),statemp,ista2
-c2070   format(f10.4, f10.4,2x, a4, 2x, i3)
-	     endif
-	  enddo
-	  if (istanum(ista).eq.0) then
-	    write(*,*) statemp, ista
-	    write(*,*) 'WARNING ', statemp,' not in station list'
-	  endif
-c          write(13,'(a)') fn(iev,ista)
-c          rewind (13)
-	  
-c ********************************modify here***********************************
-	  
+
 c          read (13,201) dummy1, nettemp
 c	  if (index(nettemp,'NR')) then
 c	     rewind(13)
@@ -231,13 +245,14 @@ c
          
           do ifreq = 1, nfreq
             call frt(tdata, freq(ifreq), staamp(iev,ista,ifreq),
-     1                      staph(iev,ista,ifreq),
-     1                      nstapts(ista),delt(ista))
+     1               staph(iev,ista,ifreq), nstapts(ista),delt(ista))
+
 c  correct amplitudes for geometrical spreading, attenuation and station factor
             attneffect= exp(cattnfac*(stadist(iev,ista)-
      1                                         stadist(iev,1)))
 c            attneffect= exp(attnfac(ifreq)*(stadist(iev,ista)-
 c     1                                         stadist(iev,1)))
+
 c            staamp(iev,ista,ifreq)= staamp(iev,ista,ifreq)*attneffect
 c     1         *(geomsprd(ista)/stacor(istanum(ista)))
             staamp(iev,ista,ifreq)= staamp(iev,ista,ifreq)*attneffect
@@ -246,21 +261,22 @@ c     1         *(geomsprd(ista)/stacor(istanum(ista)))
           enddo  
         enddo
       enddo
+
 c  end of input loop over events
 c  output loop over events
       do iev = 1, nevents
         write (11,*) iev, idnum(iev)
         do ista = 1, nsta(iev)
           write(11,*) beg(iev,ista)
-       write (11,*) stadist(iev,ista),staazi(iev,ista), bazi(iev,ista),
-     1    stadelt(iev,ista), stalat(iev,ista), stalon(iev,ista)
+          write (11,*) stadist(iev,ista), staazi(iev,ista), bazi(iev,ista),
+     1                 stadelt(iev,ista), stalat(iev,ista), stalon(iev,ista)
           write(11,*) (staamp(iev,ista,ifreq),staph(iev,ista,ifreq),
-     1                               ifreq=1,nfreq)
+     1                 ifreq=1,nfreq)
         enddo
       enddo
-      close(unit = 13,status='delete')
       close(unit = 11)
 c      close(unit = 12)
+      close(unit = 13,status='delete')
       close(unit = 14)
       close(unit = 18)
       end
